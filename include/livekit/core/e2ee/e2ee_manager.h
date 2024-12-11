f59b24508cda4c76aea7667390f045e69b96b3a9
zesun <sunze1@foxmail.com>
2024-12-11 23:04:38 +0800

    add sdk core file;

--- include/livekit/core/e2ee/e2ee_manager.h
+++ include/livekit/core/e2ee/e2ee_manager.h
@@ -15,25 +15,14 @@
  *limitations under the License.
  */
 
-#include "livekit/core/livekit_client.h"
-#include "detail/signal_client.h"
+#pragma once
 
-#include <api/peer_connection_interface.h>
-#include <functional>
-#include <iostream>
-#include <memory>
+#ifndef _LKC_CORE_E2EE_E2EE_MANAGER_H_
+#define _LKC_CORE_E2EE_E2EE_MANAGER_H_
 
 namespace livekit {
 namespace core {
-void TestWebrtc() {
-	webrtc::PeerConnectionInterface::IceTransportsType aa;
-	return;
 }
-
-bool Test() {
-	auto option = SignalClientOption();
-	auto signal_client = SignalClient::Create("ws://localhost:8080/ws", "aaa", option);
-	return true;
-}
-} // namespace core
 } // namespace livekit
+
+#endif //
