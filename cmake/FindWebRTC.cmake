941bc79abe343d100f2375421a342adf3ad99a7a
zesun <sunze1@foxmail.com>
2024-12-30 23:24:39 +0800

    switch the webrtc library download repository

--- cmake/FindWebRTC.cmake
+++ cmake/FindWebRTC.cmake
@@ -1,8 +1,38 @@
 # Find WebRTC include path
 
-set(WEBRTC_DOWNLOAD_URL "https://github.com/livekit/rust-sdks/releases/download/webrtc-dac8015-6/webrtc-win-x64-release.zip")
+set(WEBRTC_HOST_URL "https://github.com/zesun96/libwebrtc/releases/download/webrtc-dac8015-3")
 
-set(WEBRTC_DIR "${CMAKE_SOURCE_DIR}/deps/libwebrtc")
+if (CMAKE_SYSTEM_NAME MATCHES "Windows")
+  set(WEBRTC_PLATFORM "win")
+elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
+  set(WEBRTC_PLATFORM "linux")
+else()
+  set(WEBRTC_PLATFORM "Unknown")
+endif()
+
+## Detect TARGET_ARCH ##
+if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "i386|i686|amd64|x86_64|AMD64")
+	if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+		set(WEBRTC_ARCH "x64")
+	else(CMAKE_SIZEOF_VOID_P EQUAL 8)
+		set(WEBRTC_ARCH "x86")
+	endif()
+elseif (${CMAKE_SYSTEM_PROCESSOR} MATCHES "^aarch64")
+	set(WEBRTC_ARCH "arm64")
+elseif (${CMAKE_SYSTEM_PROCESSOR} MATCHES "^arm")
+	set(WEBRTC_ARCH "arm32")
+else()
+	message(FATAL_ERROR "Not a valid processor" ${CMAKE_SYSTEM_PROCESSOR})
+endif()
+  
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+  set(WEBRTC_BUILD_TYPE "debug")
+else()
+  set(WEBRTC_BUILD_TYP "release")
+endif()
+
+set(WEBRTC_DOWNLOAD_URL "${WEBRTC_HOST_URL}/webrtc-${WEBRTC_PLATFORM}-${WEBRTC_ARCH}-${WEBRTC_BUILD_TYPE}.zip")
+message(STATUS "WEBRTC_DOWNLOAD_URL: ${WEBRTC_DOWNLOAD_URL}")
 
 # download libwebrtc
 FetchContent_Declare(
